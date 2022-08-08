11430c5190bd9725184d8a0522c682ec3ff4d4ef
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-08-08 10:14:10 -0500

    learning in progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import Todo from "./components/Todo";
+import { Route ,Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
 
 
 function App() {
+  // our domain is local host 3000/
   return (
     <div>
-      <hi>My Todos</hi>
-     <Todo text="Learn React" />
-     <Todo text ='Master React'/>
-     <Todo text='Explore the Full React Course' />
+      <Switch>
+     <Route path='/' exact={true}>
+      <AllMeetupsPage/>
+    </Route>
 
+    <Route path='/new-meetup'>
+      <NewMeetupPage/>
+    </Route>
+
+    <Route path='/favorites'>
+      <FavoritesPage/>
+    </Route>
+    </Switch>
     </div>
   );
 }
---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,6 @@
+function AllMeetupsPage () {
+    return(
+        <div>All Meetups Page</div>
+    )
+}
+export default AllMeetupsPage;
